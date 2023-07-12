6fe540b8e8b5ae1982e5505e17ac85a7fa2f89c6
Michal Seják <sejakm@ntis.zcu.cz>
2023-07-12 13:11:54 +0200

    Added a script for downloading and processing all datasets at once.

--- dataset_stats.py
+++ dataset_stats.py
@@ -2,6 +2,7 @@ import os
 
 from h5_pt_dataloader import MultiFileHDF5ECGHandle
 
+print("location,number_of_ecgs,number_of_patients")
 for ds in os.listdir('datasets'):
     full_path = os.path.join('datasets', ds)
     h = MultiFileHDF5ECGHandle(full_path)
